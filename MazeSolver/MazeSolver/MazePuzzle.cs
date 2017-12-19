6fd5d3c52011a0ff9a3269ee1b92d6eadc9989cb
AbrahamOdamteng <abraham.odamteng.developer@gmail.com>
2017-12-19 15:59:52 +0000

    Added README.txt and added comments to MazePuzzle Class

--- MazeSolver/MazeSolver/MazePuzzle.cs
+++ MazeSolver/MazeSolver/MazePuzzle.cs
@@ -10,6 +10,10 @@ namespace MazeSolver
         internal Point EndPoint { get; set; }
         internal int[,] Maze { get; set; }
 
+        /// <summary>
+        /// Load an instance of the maze puzzle from mazeFilePath.
+        /// </summary>
+        /// <param name="mazeFilePath">The filepath of the maze to load</param>
         public MazePuzzle(string mazeFilePath)
         {
             var lines = File.ReadAllLines(mazeFilePath);
@@ -20,6 +24,15 @@ namespace MazeSolver
             Maze = LoadMaze(lines.Skip(3).ToArray(), mazeSize.X, mazeSize.Y);
         }
 
+        /// <summary>
+        /// Convert the single dimension string array (string[]) representation of the maze, 
+        /// into a multidemensional int (int[,]) representation.
+        /// </summary>
+        /// <param name="mazeArray"></param>
+        /// <param name="mazeWidth">the width of the integer array </param>
+        /// <param name="mazeHeight">the height of the integer array</param>
+        /// <returns>A multidimensional integer array that represents the maze</returns>
+        /// <exception cref="ArgumentException">If mazeWidth or mazeHeight is less than 1</exception>  
         internal static int[,] LoadMaze(string[] mazeArray, int mazeWidth, int mazeHeight)
         {
             if(mazeWidth < 1) throw new ArgumentException("Maze width must be greater than zero");
@@ -31,8 +44,6 @@ namespace MazeSolver
             }
 
             var maze = new int[mazeHeight, mazeWidth];
-
-            
 
             for (int y = 0; y < mazeHeight; y++)
             {
@@ -51,6 +62,12 @@ namespace MazeSolver
             return maze;
         }
 
+        /// <summary>
+        /// Parses the parameters in the maze text files.
+        /// </summary>
+        /// <param name="input">A string of the form '# #' where each # is an integer</param>
+        /// <returns>A point containing the two values in the input string</returns>
+        /// <exception cref="ArgumentException">If the input string is not in the required format</exception> 
         internal static Point ParseMazeParameters(string input)
         {
             var inputArray = input.Split();
